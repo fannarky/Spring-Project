0d65c4b292f44b97289ed7b63b304b220b575bc5
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-08-26 20:53:16 -0300

    nova aula

--- AgendaDeObjetivos/src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/UsuariosController.java
+++ AgendaDeObjetivos/src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/UsuariosController.java
@@ -8,21 +8,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
 public class UsuariosController {
 
-	private List<Usuario> usuario;
+	private List<Usuario> usuarios;
 	
 	public UsuariosController() {
-		this.usuario = obterTodosUsuarios();
+		this.usuarios = obterTodosUsuarios();
 	}
 	
 	@GetMapping("/usuarios/nome/{nome}")
 	public ResponseEntity<List<Usuario>> obterPorNome(@PathVariable("nome") String nome){
 		List<Usuario> users = new ArrayList<>();
-		for(Usuario user : this.usuario) {
+		for(Usuario user : this.usuarios) {
 			
 			if(user.getNome().equals(nome))
 				users.add(user);
@@ -39,7 +41,7 @@ public class UsuariosController {
 	public ResponseEntity<List<Usuario>> obterPorIdade(@PathVariable("idade") Integer idade){
 		List<Usuario> usersPorIdade = new ArrayList<>();
 		
-		for(Usuario userid : this.usuario) {
+		for(Usuario userid : this.usuarios) {
 			if(userid.getIdade() == idade)
 				usersPorIdade.add(userid);
 		}
@@ -56,10 +58,24 @@ public class UsuariosController {
 	}
 	
 	private List<Usuario> obterTodosUsuarios(){
-		return Arrays.asList(
+		/*return Arrays.asList(
 				new Usuario("Rodrigo", 39),
 				new Usuario("Joao", 22),
 				new Usuario("Jose", 30)
-			);
+			);*/
+		
+		ArrayList<Usuario> usuarios = new ArrayList<>();
+		
+		usuarios.add(new Usuario("Rodrigo", 39));
+		usuarios.add(new Usuario("Joao", 22));
+		usuarios.add(new Usuario("Jose", 30));
+		
+		return usuarios;
+	}
+	
+	@PostMapping("/usuarios")
+	public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+		this.usuarios.add(usuario);
+		return ResponseEntity.ok(usuario);
 	}
 }
